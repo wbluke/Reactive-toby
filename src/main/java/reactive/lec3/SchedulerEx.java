ac77b14d996ff839912e3c3de8df34d10d50efbf
wbluke <pwb0127@gmail.com>
2022-03-17 08:25:35 +0900

    SchedulerEx

---
+++ src/main/java/reactive/lec3/SchedulerEx.java
@@ -0,0 +1,92 @@
+package reactive.lec3;
+
+import lombok.extern.slf4j.Slf4j;
+import org.reactivestreams.Publisher;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Slf4j
+public class SchedulerEx {
+
+    public static void main(String[] args) {
+        Publisher<Integer> publisher = sub -> {
+            sub.onSubscribe(new Subscription() {
+                @Override
+                public void request(long n) {
+                    log.debug("request()");
+                    sub.onNext(1);
+                    sub.onNext(2);
+                    sub.onNext(3);
+                    sub.onNext(4);
+                    sub.onNext(5);
+                    sub.onComplete();
+                }
+
+                @Override
+                public void cancel() {
+
+                }
+            });
+        };
+
+//        Publisher<Integer> subOnPub = sub -> {
+//            ExecutorService es = Executors.newSingleThreadExecutor();
+//            es.execute(() -> publisher.subscribe(sub));
+//        };
+
+        Publisher<Integer> pubOnSub = sub -> {
+            publisher.subscribe(new Subscriber<>() {
+                ExecutorService es = Executors.newSingleThreadExecutor();
+
+                @Override
+                public void onSubscribe(Subscription s) {
+                    sub.onSubscribe(s);
+                }
+
+                @Override
+                public void onNext(Integer integer) {
+                    es.execute(() -> sub.onNext(integer));
+                }
+
+                @Override
+                public void onError(Throwable t) {
+                    es.execute(() -> sub.onError(t));
+                }
+
+                @Override
+                public void onComplete() {
+                    es.execute(() -> sub.onComplete());
+                }
+            });
+        };
+
+        pubOnSub.subscribe(new Subscriber<>() {
+            @Override
+            public void onSubscribe(Subscription subscription) {
+                log.debug("onSubscribe");
+                subscription.request(Long.MAX_VALUE);
+            }
+
+            @Override
+            public void onNext(Integer integer) {
+                log.debug("onNext : {}", integer);
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                log.debug("onError : {}", t.getMessage());
+            }
+
+            @Override
+            public void onComplete() {
+                log.debug("onComplete");
+            }
+        });
+
+        System.out.println("exit");
+    }
+
+}
